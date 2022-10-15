4f2a3385e3b5c353bc4c0a8337a751cbcfdf66dc
brayancoelho <brayan.aquino@sptech.school>
2022-10-15 14:24:08 -0300

    JS da index da dashboard

--- site/src/models/salaModel.js
+++ site/src/models/salaModel.js
@@ -3,7 +3,12 @@ var database = require("../database/config");
 function cadastrar(nome, fkInstituicao) {
     return database.executar(`INSERT INTO sala VALUES (null, '${nome}', ${fkInstituicao});`);
 }
+function getSalas(idInstituicao){
+    // return database.executar(`SELECT * FROM vw_manutencao_por_sala WHERE idInstituicao = ${idInstituicao};`);
+    return database.executar(`SELECT * FROM sala WHERE fkInstituicao = ${idInstituicao};`);
+}
 
 module.exports = {
-    cadastrar
+    cadastrar,
+    getSalas
 }

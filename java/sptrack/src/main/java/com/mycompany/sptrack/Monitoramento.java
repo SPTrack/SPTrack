2366e64b926e8f74a9bfaea0067def9ab5cb6f5c
brayancoelho <brayan.aquino@sptech.school>
2022-10-16 18:41:45 -0300

    Atualizou o java

--- java/sptrack/src/main/java/com/mycompany/sptrack/Monitoramento.java
+++ java/sptrack/src/main/java/com/mycompany/sptrack/Monitoramento.java
@@ -34,13 +34,10 @@ public class Monitoramento extends javax.swing.JFrame {
         lblCPU = new javax.swing.JProgressBar();
         jLabel3 = new javax.swing.JLabel();
         lblRam = new javax.swing.JProgressBar();
-        jLabel4 = new javax.swing.JLabel();
-        lblHardDisk = new javax.swing.JProgressBar();
         jLabel5 = new javax.swing.JLabel();
         lblTemp = new javax.swing.JProgressBar();
         jLabel6 = new javax.swing.JLabel();
         lblSO = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         lblLogo = new javax.swing.JLabel();
@@ -81,20 +78,6 @@ public class Monitoramento extends javax.swing.JFrame {
         lblRam.setStringPainted(true);
         lblRam.setForeground(Color.black);
 
-        jLabel4.setFont(new java.awt.Font("Liberation Sans", 1, 18)); // NOI18N
-        jLabel4.setText("DISCO RÍGIDO");
-
-        //Disco grupoDeDiscos = looca.getGrupoDeDiscos().getDiscos();
-
-        //int discoValorEmUso = looca.getGrupoDeDiscos().getTamanhoTotal().intValue();
-        //lblHardDisk.setValue(discoValorEmUso);
-        //String UsoDisco = String.format("%.1f %%", discoValorEmUso);
-
-        //lblHardDisk.setString(DiscoValorEmUso);
-        //lblHardDisk.setValue(DiscoValorEmUso);
-        //lblHardDisk.setStringPainted(true);
-        //lblHardDisk.setForeground(Color.black);
-
         jLabel5.setFont(new java.awt.Font("Liberation Sans", 1, 18)); // NOI18N
         jLabel5.setText("TEMPERATURA");
 
@@ -113,9 +96,6 @@ public class Monitoramento extends javax.swing.JFrame {
 
         Sistema sistema = this.looca.getSistema();
         lblSO.setText(String.format("%s - %s", sistema.getFabricante(), sistema.getSistemaOperacional()));
-
-        jLabel7.setFont(new java.awt.Font("Liberation Sans", 1, 18)); // NOI18N
-        jLabel7.setText("Processos mais pesados");
 
         jPanel1.setBackground(new java.awt.Color(1, 1, 1));
         jPanel1.setForeground(new java.awt.Color(254, 254, 254));
@@ -167,24 +147,19 @@ public class Monitoramento extends javax.swing.JFrame {
                                     .addComponent(lblRam, javax.swing.GroupLayout.PREFERRED_SIZE, 458, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel4)
-                                    .addComponent(lblHardDisk, javax.swing.GroupLayout.PREFERRED_SIZE, 458, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel5)
-                                    .addComponent(jLabel6)))
+                                .addComponent(jLabel6))
                             .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addComponent(lblSO, javax.swing.GroupLayout.PREFERRED_SIZE, 458, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel7)
-                                    .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 313, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addGap(0, 168, Short.MAX_VALUE)))
+                                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 313, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 71, Short.MAX_VALUE)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel5)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -199,21 +174,15 @@ public class Monitoramento extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblRam, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(lblHardDisk, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
                 .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(lblTemp, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel6)
-                .addGap(75, 75, 75)
+                .addGap(44, 44, 44)
                 .addComponent(lblSO, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel7)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, 169, Short.MAX_VALUE)
+                .addGap(36, 36, 36)
+                .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -254,14 +223,11 @@ public class Monitoramento extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JProgressBar lblCPU;
-    private javax.swing.JProgressBar lblHardDisk;
     private javax.swing.JLabel lblLogo;
     private javax.swing.JProgressBar lblRam;
     private javax.swing.JLabel lblSO;

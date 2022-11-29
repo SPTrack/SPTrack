7736cce2399794f9b95d30705ab1117d30bb6f25
eduardanv <maria.neves@sptech.school>
2022-11-29 15:21:02 -0300

    chartjs on screen calls

--- site/public/js/dashboard/chamados.js
+++ site/public/js/dashboard/chamados.js
@@ -1,6 +1,30 @@
 qtdTotal = 0
 qtdConcluido = 0
 
+
+  const ctx = document.getElementById('myChart');
+
+ var grafico = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+ 
+
 function get(){
     fetch("/medidas/pegarInfoChamado", {
         method: "POST",

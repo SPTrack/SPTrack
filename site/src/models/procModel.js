ac3a628c9c88aa493fa426e75d0750be4e39f7e7
gabs-sanchez <gabriel.mainente@sptech.school>
2022-12-04 13:23:01 -0300

    individual com o banco local

--- site/src/models/procModel.js
+++ site/src/models/procModel.js
@@ -4,13 +4,18 @@ function cadastro(sisop,nameP,tipoP){
     return database.executar(`insert into processos(so,arquivos,tipoProcesso) values('${sisop}','${nameP}','${tipoP}');`);
 }
 function getProcessosMortos(){
-    return database.executar("select nome,horas  from processosMortos order by horas desc OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;");
+    if(process.env.AMBIENTE_PROCESSO == 'producao'){
+    return database.executar("select nome,horas  from processosMortos order by horas desc OFFSET 0 ROWS FETCH NEXT 20 ROWS ONLY;");
+    }
+    else{
+        return database.executar("select nome,horas  from processosMortos order by horas desc limit 20");
+    }
 }
 function getProcessos(){
     return database.executar("select * from processos;");
 }
 function setCategoria(id,soP,nomeP,tipoP){
-    return database.executar(`update processos set so='${soP}',arquivos='${nomeP}',tipoProcesso = '${tipoP}' where idLeitura = '${id}' ;`);
+    return database.executar(`update processos set so='${soP}',arquivos='${nomeP}',tipoProcesso = '${tipoP}' where idLeitura = ${id} ;`);
 }
 module.exports = {
     cadastro,
